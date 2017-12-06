ae44fb3300b29fb578bb9b97ea7c9d9d76342ae0
Gary Dean Jenkins <gary.jenkins@klowd.com>
2017-12-06 10:22:58 -0700

    comments init_db

--- init_db.js
+++ init_db.js
@@ -1,10 +1,66 @@
 var argv = require('optimist')
-    .usage('Usage: $0 -host [mysql_hostname] -user [mysql_user] -password [mysql_paswword] -file [absolute sql file path]')
+    .usage('Usage: $0 -host [mysql_hostname] -user [mysql_user] -password [mysql_paswword] -file [sql file path]')
     .demand(['host','user','password','file'])
     .argv;
 
-var mysql = require('mysql');
+//Get the SQL File as an array of strings and no empty lines
 var fs = require("fs");
+var text = fs.readFileSync(argv.file).toString();
+var lines = text.split("\n");
+
+lines = lines.filter(line => line.length > 0);
+
+
+//Convert Array of lines into an array of SQL Statements
+//State machine
+var state = 0;
+var curr = '';
+var sep = '';
+var sqlArry = [];
+
+lines.forEach(function (line) {
+    switch (state) {
+        case 0: 
+            //Beginning of a new SQL statement
+            if (line.search(/DELIMITER/) > -1) {
+                //Ignoring the first line since don't need the DELIMITER when running single statements
+                state = 2;
+            } else {
+                curr = curr + sep + line;
+                sep = ' ';
+                state = line.search(/\;/) === -1 ? 1 : 0;
+            }
+            break;
+        case 1: 
+            //Building Regular Statemnt
+            curr = curr + sep + line;
+            state = (line.search(/\;/) === -1) ? 1 : 0;
+            break;
+        case 2: 
+            //Building Delimiter|Create|Function
+            curr = curr + sep + line;
+            sep = ' ';
+            if (line.search(/END/) > -1) {
+                curr = curr.replace(/END.*$/,'END');
+                state = 3;
+            }
+            break;
+        case 3: 
+            //Ignoring after 'END' in Delimiter|Create|Function
+            state = (line.search(/DELIMITER/) > -1) ? 0 : 3;
+            break;
+    }
+    //If state has been set to 0, then go ahead and reset and save SQL statement that has been created
+    if (state === 0) {
+        sqlArry.push(curr);
+        curr = '';
+        sep = '';
+    }
+});
+
+// Now let's send the sql to the mysql server
+
+var mysql = require('mysql');
 
 var connection_params = {
     host: argv.host,
@@ -13,65 +69,12 @@ var connection_params = {
     multipleStatements: false
 };
 
-var text = fs.readFileSync(argv.file).toString();
-var lines = text.split("\n");
-
-lines = lines.filter(line => line.length > 0);
-
-var state = 0;
-var curr = '';
-var sep = '';
-var sqlArry = [];
-
-lines.forEach(function (line) {
-    switch (state) {
-        case 0:
-            if (line.search(/DELIMITER/) > -1) {
-                state = 2;
-            } else {
-                curr = curr + sep + line;
-                sep = ' ';
-                state = line.search(/\;/) === -1 ? 1 : 0;
-            }
-            break;
-        case 1:
-            curr = curr + sep + line;
-            state = (line.search(/\;/) === -1) ? 1 : 0;
-            break;
-        case 2:
-            curr = curr + sep + line;
-            sep = ' ';
-            if (line.search(/END/) > -1) {
-                curr = curr.replace(/END.*$/,'END');
-                state = 3;
-            }
-            break;
-        case 3:
-            state = (line.search(/DELIMITER/) > -1) ? 0 : 3;
-            break;
-    }
-    if (state === 0) {
-        sqlArry.push(curr);
-        curr = '';
-        sep = '';
-    }
-});
-
-if (curr.length > 0) {
-    groups.push(curr);
-}
-
-sqlArry.forEach(function (item) {
-    console.log('SQL -> ' + item);
-});
-
-
 runQueries(connection_params,sqlArry);
 
+// SQL file should start with a create schema command (no database in params!)
+// SQL should include a 'use `database_name`;' command, we use it to set the database in params later
 function runQueries(params, sqlArray) {
     var sql = sqlArray.shift();
-
-    console.log(`sql = ${sql}`);
 
     var db = sql.match(/use \`(.+)\`\;/);
     if (db) {
