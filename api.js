077d6d79e96b5ea371fdb82387b735507651912a
Gary Dean Jenkins <gary.jenkins@klowd.com>
2017-12-08 09:24:10 -0700

    More updates

--- api.js
+++
@@ -1,4 +0,0 @@
-let fuzionApi = require('./node_modules/fuzion-admin-api/fuzion_api');
-
-let api = new fuzionApi();
-api.run();
--- register.js
+++
@@ -1,3 +0,0 @@
-let routeBuilder = require("./node_modules/fuzion-admin-api/router_builder.js");
-
-routeBuilder.addSource(`${__dirname}/routes`);
